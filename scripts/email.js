ede14f84a10d727c2a55ad46a58151f82ea0dde9
Skeletrox <android.svr@gmail.com>
2017-04-01 21:02:36 +0530

    update webpages with proper data

---
+++ scripts/email.js
@@ -0,0 +1,44 @@
+<section class="section--center mdl-grid site-max-width">
+      	<div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
+        <div class="mdl-card__media">
+            <img src="img/facebook.jpg" border="0" alt="" width="284" height="213">
+        </div>
+        <div class="mdl-card__title">
+        <h2 class="mdl-card__title-text">Are you a friend?</h2>
+        </div>
+        <div class="mdl-card__supporting-text">
+          I use Facebook mainly for personal connections and social networking so if you know me on a personal level, visit my Facebook!<br><br><br><br></div>
+         <div class="mdl-card__actions  mdl-card--border">
+      <a target = "_blank" href="https://www.facebook.com/Chaosbane" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent">Visit Facebook</a>
+    	</div>
+  </div> 
+  
+    <div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
+        <div class="mdl-card__media">
+            <img src="img/linkedin.jpg" border="0" alt="" width="284" height="213">
+        </div>
+        <div class="mdl-card__title">
+            <h2 class="mdl-card__title-text">Know me professionally? Or wish to?</h2>
+        </div>
+        <div class="mdl-card__supporting-text">
+  			LinkedIn is for professional connections. If you would like to view my professional profile, click the below link.
+        </div>
+         <div class="mdl-card__actions  mdl-card--border">
+      <a target = "_blank" href="https://www.linkedin.com/in/sriram-ramaswamy-5584a3137/" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent">Visit LinkedIn</a>
+    	</div>
+    </div>
+    <div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
+        <div class="mdl-card__media">
+            <img  src="img/github.jpg" border="0" alt="" width="284" height="213">
+        </div>
+        <div class="mdl-card__title">
+            <h2 class="mdl-card__title-text">Do you want to see my code?</h2>
+        </div>
+        <div class="mdl-card__supporting-text">
+         I upload a lot of code on github, many being standalone programs that are just grouped together. Visit the below link if you'd like to check them out or have me work on one of your projects as well.
+        </div>
+         <div class="mdl-card__actions  mdl-card--border">
+        <a target = "_blank" href="http://github.com/Skeletrox" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent">Visit github</a>
+    	</div>
+    </div>
+      	</section>
